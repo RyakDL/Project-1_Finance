7d18249985df9249b7f3502a5e83e446a35b1297
RyakDL <rdlund@berkeley.edu>
2023-07-13 21:25:33 -0700

    main_active

--- Main.py
+++ Main.py
@@ -1,1 +1,14 @@
-Main
+import pandas as pd
+
+#merge data files
+AZ_Employment ="EMPLOYAZ.csv"
+AZ_Assets = "AZTAST.csv"
+
+# Read the data and the study results
+AZ_Employment = pd.read_csv(AZ_Employment)
+AZ_Assets = pd.read_csv(AZ_Assets)
+
+# Combine the data into a single DataFrame
+Combined_AZ = pd.merge(AZ_Employment, AZ_Assets, how ='right')
+
+Combined_AZ
